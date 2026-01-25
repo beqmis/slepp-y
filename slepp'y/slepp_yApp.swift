6fbf3b59fe487c1626c43ad8297a2188dbf4b7fa
Яков Демиденко <akovdemidenko@MacBook-Pro-Akov.local>
2026-01-25 22:48:46 +0300

    Initial commit

---
+++ slepp'y/slepp_yApp.swift
@@ -0,0 +1,17 @@
+//
+//  slepp_yApp.swift
+//  slepp'y
+//
+//  Created by Яков Демиденко on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct slepp_yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
